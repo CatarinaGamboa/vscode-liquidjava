8bef466f0fd7162a88e89e8d106678df9cbbd8b0
Catarina Ventura Gamboa <cgamboa@lasige.di.fc.ul.pt>
2021-05-12 12:31:58 +0100

    Add translation of url to charset in workspace path (came c%3A instead of c:)

--- server/src/main/java/LJDiagnostics.java
+++ server/src/main/java/LJDiagnostics.java
@@ -1,3 +1,5 @@
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +11,6 @@ import org.eclipse.lsp4j.PublishDiagnosticsParams;
 import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.TextDocumentIdentifier;
 import org.eclipse.lsp4j.TextDocumentItem;
-import org.eclipse.lsp4j.VersionedTextDocumentIdentifier;
 
 import repair.regen.api.CommandLineLauncher;
 import repair.regen.utils.ErrorEmitter;
@@ -29,7 +30,8 @@ public class LJDiagnostics {
 	
 	
 	private static Optional<PublishDiagnosticsParams> verify(String uri){
-		String u = "C:/"+uri.substring(12);
+		String u = uri.substring(8);
+		u = convertUTFtoCharacters(u);
 		ErrorEmitter ee;
 		try {
 			ee = CommandLineLauncher.launch(u);
@@ -62,99 +64,13 @@ public class LJDiagnostics {
 	
 	
 
-//	public static PublishDiagnosticsParams createDiagnostics(TextDocumentIdentifier textDocumentItem) {
-//        PublishDiagnosticsParams diagnosticsParams = new PublishDiagnosticsParams();
-//        List<Diagnostic> diagnostics = new ArrayList<>();
-//        Range range = new Range(
-//                new Position(0, 1),
-//                new Position(0, 50)
-//        );
-////        exception.printStackTrace();
-//        diagnostics.add(new Diagnostic(range, "Error",  DiagnosticSeverity.Error, "Error processing file"));
-//        diagnosticsParams.setDiagnostics(diagnostics);
-//        diagnosticsParams.setUri(textDocumentItem.getUri());
-////        setBasicDiagnostics(document, diagnosticsParams);
-//        return diagnosticsParams;
-//    }
-
-//    private static void setBasicDiagnostics(TextDocumentIdentifier document, PublishDiagnosticsParams diagnosticsParams) {
-//        diagnosticsParams.setUri(document.getUri());
-//        if(document instanceof VersionedTextDocumentIdentifier) {
-//        	diagnosticsParams.setVersion(((VersionedTextDocumentIdentifier) document).getVersion ());
-//        }
-//    }
-////
-//	public static Optional<PublishDiagnosticsParams> checkDiagnostics() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-
-
-//    public static PublishDiagnosticsParams createDiagnostics(TextDocumentIdentifier document, ANTLRParserErrors errors) {
-//        PublishDiagnosticsParams diagnosticsParams = new PublishDiagnosticsParams();
-//        List<Diagnostic> diagnostics = new ArrayList<>();
-//        for(ANTLRParserMessage warning:errors.getWarnings()) {
-//            diagnostics.add(createParserDiagnostic(warning, DiagnosticSeverity.Warning));
-//        }
-//        for(ANTLRParserMessage error:errors.getErrors()) {
-//            diagnostics.add(createParserDiagnostic(error, DiagnosticSeverity.Error));
-//        }
-//TODO: replace ANTLRParserErrors with a better class
-// if(document.getExceptionDuringProcessing() != null) {
-//            //TODO: stacktrace? some extra message to indicate context?
-//            String message = document.getExceptionDuringProcessing().getMessage() == null ? document.getExceptionDuringProcessing().toString() : document.getExceptionDuringProcessing().getMessage();
-//            diagnostics.add(new Diagnostic(new Range(new Position(0, 1), new Position(0, 50)), message));
-//        }
-//
-//        diagnosticsParams.setDiagnostics(diagnostics);
-//        setBasicDiagnostics(document, diagnosticsParams);
-//        return diagnosticsParams;
-//    }
-
-//    public static PublishDiagnosticsParams createDiagnosticsFromValidationResult(TextDocumentIdentifier textDocumentItem, DocumentInformation docInfo, ValidationResult validationResult) {
-//        PublishDiagnosticsParams diagnosticsParams = new PublishDiagnosticsParams();
-//        List<Diagnostic> diagnostics = new ArrayList<>();
-//
-//        if(validationResult != null) {
-//            if(validationResult.hasWarningsOrErrors()) {
-//                for(ValidationMessage message:validationResult.getErrors()) {
-//                    DocumentSymbol documentSymbol = null;
-//                    if(message.getPathInArchetype() != null) {
-//                        documentSymbol = docInfo.lookupCObjectOrAttribute(message.getPathInArchetype());
-//                    }
-//                    if(documentSymbol != null) {
-//                        Range range = documentSymbol.getSelectionRange();
-//                        diagnostics.add(new Diagnostic(range, toMessage(message), message.isWarning() ? DiagnosticSeverity.Warning : DiagnosticSeverity.Error, "ADL validation", message.getType().toString()));
-//                    } else {
-//                        Range range = new Range(
-//                                new Position(0, 1),
-//                                new Position(0, 50)
-//                        );
-//                        diagnostics.add(new Diagnostic(range, toMessage(message), message.isWarning() ? DiagnosticSeverity.Warning : DiagnosticSeverity.Error, "ADL validation", message.getType().toString()));
-//                    }
-//                }
-//            }
-//        }
-//        diagnosticsParams.setDiagnostics(diagnostics);
-//        setBasicDiagnostics(textDocumentItem, diagnosticsParams);
-//        return diagnosticsParams;
-//    }
-
-//    private static Diagnostic createParserDiagnostic(ANTLRParserMessage error, DiagnosticSeverity warning) {
-//        Range range = new Range(
-//                new Position(error.getLineNumber()-1, error.getColumnNumber()),
-//                new Position(error.getLineNumber()-1, error.getColumnNumber() + error.getLength())//TODO: archie errors do not keep the position properly
-//        );
-//
-//        return new Diagnostic(range, error.getShortMessage(), warning, "ADL2 syntax");
-//    }
-//
-//    private static String toMessage(ValidationMessage message) {
-//        if(message.getMessage() != null) {
-//            return message.getMessage();
-//        } else {
-//            return message.getType().getDescription();
-//        }
-//    }
-
+	private static String convertUTFtoCharacters(String source) {	
+		try {
+		    String result = java.net.URLDecoder.decode(source, StandardCharsets.UTF_8.name());
+		    return result;
+		} catch (UnsupportedEncodingException e) {
+		    // not going to happen - value came from JDK's own StandardCharsets
+		}
+		return null;
+	}
 }
