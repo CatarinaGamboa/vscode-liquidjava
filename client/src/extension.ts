ef657978f7444edec73d96fa914ba5c97882af94
Catarina Ventura Gamboa <cgamboa@lasige.di.fc.ul.pt>
2021-05-21 20:40:55 +0100

    Still trying to run server in client side. No sucess (more errors)

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -6,9 +6,10 @@ import * as net from 'net';
 import * as child_process from "child_process";
 
 import { workspace, Disposable, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, SettingMonitor, StreamInfo } from 'vscode-languageclient';
+import { LanguageClient, LanguageClientOptions, ServerOptions, SettingMonitor, StreamInfo, TransportKind  } from 'vscode-languageclient';
 
 import { LiquidJavaProvider } from './liquidJavaProvider';
+import { Executable } from 'vscode-languageclient/lib/client';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -26,41 +27,82 @@ export function activate(context: vscode.ExtensionContext) {
         });
 
         //Start server only if api is inside the project
-        let serverOptions;
 
-        let javaExecutablePath = findJavaExecutable('java');
-        let serverCommand = javaExecutablePath + " -jar " + path.join(context.extensionPath, '..', 'server', 'language-server-liquidjava.jar');
+        //#1
+        const { JAVA_HOME } = process.env;
+        const main: string = 'Main';
+        console.log(`Using java from JAVA_HOME: ${JAVA_HOME}`);
+        if (JAVA_HOME) {
+            // Java execution path.
+            let executable: string = path.join(JAVA_HOME,'java');
 
-        console.log("serverCommand:");
-        console.log(serverCommand);
-
-
-        serverOptions = {
-            run: {
-                command: serverCommand
-            },
-            debug: {
-                command: serverCommand
-            }
-        };
-
-        let clientOptions: LanguageClientOptions = {
-            documentSelector: ['java'],
-            synchronize: {
-                fileEvents: workspace.createFileSystemWatcher('**/*.java')
-            }
-        };
-
-         // Create the language client and start the client.
-    let lc = new LanguageClient('LiquidJava Server', serverOptions, clientOptions);
-
-	lc.trace = Trace.Verbose;
+            // C:\Program Files\Java\jdk1.8.0_231\bin\bin\java
     
-    let disposable = lc.start();
+            // path to the launcher.jar
+            let classPath = path.join(__dirname, '..', 'server', 'language-server-liquidjava.jar');
+            const args: string[] = ['-cp', classPath];
     
-    // Push the disposable to the context's subscriptions so that the 
-    // client can be deactivated on extension deactivation
-    context.subscriptions.push(disposable);
+            // Set the server options 
+            // -- java execution path
+            // -- argument to be pass when executing the java command
+            let serverOptions: ServerOptions = {
+                command: executable,
+                args: [...args, main],
+                options: {}
+            };
+    
+            // Options to control the language client
+            let clientOptions: LanguageClientOptions = {
+                documentSelector: ['java'],
+                synchronize: {
+                    fileEvents: workspace.createFileSystemWatcher('**/*.java')
+                }
+            };
+    
+            // Create the language client and start the client.
+            let disposable = new LanguageClient('LiquidJava Server', serverOptions, clientOptions).start();
+    
+            // Disposables to remove on deactivation.
+            context.subscriptions.push(disposable);
+        }
+
+
+        //#2
+        // let javaExecutablePath = findJavaExecutable('java');
+        // //// let serverCommand = javaExecutablePath + " -jar ";
+        // let extensionDir =  __dirname;//context.extensionPath;
+        // let classPath = path.join(extensionDir, '..', 'server', 'language-server-liquidjava.jar');
+        // const args: string[] = ['-cp', classPath];
+        // const main: string = 'Main';
+
+        // let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
+
+        // let serverOptions:ServerOptions = {
+        //     command: javaExecutablePath, 
+        //     args: [...args, main],
+        //     transport: TransportKind.ipc
+        // };
+        // console.log(serverOptions);
+
+      
+
+        // let clientOptions: LanguageClientOptions = {
+        //     documentSelector: ['java'],
+        //     synchronize: {
+        //         fileEvents: workspace.createFileSystemWatcher('**/*.java')
+        //     }
+        // };
+
+        //  // Create the language client and start the client.
+        // let lc = new LanguageClient('LiquidJava Server', serverOptions, clientOptions);
+
+        // lc.trace = Trace.Verbose;
+        
+        // let disposable = lc.start();
+        
+        // // Push the disposable to the context's subscriptions so that the 
+        // // client can be deactivated on extension deactivation
+        // context.subscriptions.push(disposable);
         
 
     });
