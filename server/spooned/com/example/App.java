143b5841a488c8202136be352f83fff5d32587ab
Catarina Gamboa <catarina.vg@hotmail.com>
2025-04-11 10:59:15 +0100

    update gitognore

---
+++ server/spooned/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(java.lang.String[] args) {
+        @liquidjava.specification.Refinement("a > 0")
+        int a = 1;
+    }
+}
