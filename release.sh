e83199172a575b391ec41641dcfaa17335c42052
Ricardo Costa <rcosta.ms358@gmail.com>
2025-10-19 11:49:41 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,6 +2,12 @@
 
 VERSION=$1
 
+# check if version argument provided
+if [ -z "$VERSION" ]; then
+    echo "Usage: $0 1.2.3"
+    exit 1
+fi
+
 # check valid version format
 if [[ ! $VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "Version must be in format 1.2.3"
