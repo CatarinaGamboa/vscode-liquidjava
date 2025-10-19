e8b9e4ac0463999e12535a443a9426efa5f7d525
Ricardo Costa <rcosta.ms358@gmail.com>
2025-10-19 11:51:36 +0100

    Webpack Bundling

---
+++ client/webpack.config.js
@@ -0,0 +1,42 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node', // vscode extensions run in a Node.js context
+  mode: 'none', // leave the source code as close as possible to the original
+
+  entry: './src/extension.ts', // the entry point of this extension
+  output: {
+    // the bundle is stored in the 'dist' folder
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
+module.exports = config;
